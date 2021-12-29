d0c7175affc713b66911a7d686309a586792b3e1
912-Dragos-Serenus <serenus.dragos@stud.ubbcluj.ro>
2021-12-29 22:32:00 +0200

    * Add GUI Anchoring for form resizing * Add search bar with no current functionality

--- WinDeviceManagerUI/Form1.Designer.cs
+++ WinDeviceManagerUI/Form1.Designer.cs
@@ -28,8 +28,8 @@ namespace HW_Lib_Test
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("");
-            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem3 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem4 = new System.Windows.Forms.ListViewItem("");
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
@@ -38,11 +38,13 @@ namespace HW_Lib_Test
             this.HardwareId = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.listdevices = new System.Windows.Forms.ListView();
             this.Status = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(259, 665);
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(259, 705);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(89, 29);
             this.button1.TabIndex = 1;
@@ -52,7 +54,8 @@ namespace HW_Lib_Test
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(518, 665);
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(518, 705);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(89, 29);
             this.button2.TabIndex = 2;
@@ -63,7 +66,7 @@ namespace HW_Lib_Test
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(31, 10);
+            this.label1.Location = new System.Drawing.Point(31, 13);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(109, 13);
             this.label1.TabIndex = 4;
@@ -86,19 +89,23 @@ namespace HW_Lib_Test
             // 
             // listdevices
             // 
+            this.listdevices.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.listdevices.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.CommonName,
             this.FriendlyName,
             this.HardwareId,
             this.Status});
             this.listdevices.FullRowSelect = true;
+            this.listdevices.HideSelection = false;
             this.listdevices.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
-            listViewItem1,
-            listViewItem2});
-            this.listdevices.Location = new System.Drawing.Point(34, 26);
+            listViewItem3,
+            listViewItem4});
+            this.listdevices.Location = new System.Drawing.Point(34, 36);
             this.listdevices.MultiSelect = false;
             this.listdevices.Name = "listdevices";
-            this.listdevices.Size = new System.Drawing.Size(858, 615);
+            this.listdevices.Size = new System.Drawing.Size(858, 645);
             this.listdevices.TabIndex = 5;
             this.listdevices.UseCompatibleStateImageBehavior = false;
             this.listdevices.View = System.Windows.Forms.View.Details;
@@ -110,11 +117,21 @@ namespace HW_Lib_Test
             this.Status.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.Status.Width = 93;
             // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(147, 10);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(745, 20);
+            this.textBox1.TabIndex = 6;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(922, 723);
+            this.ClientSize = new System.Drawing.Size(922, 763);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.listdevices);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
@@ -137,6 +154,7 @@ namespace HW_Lib_Test
         private System.Windows.Forms.ColumnHeader HardwareId;
         private System.Windows.Forms.ListView listdevices;
         private System.Windows.Forms.ColumnHeader Status;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
